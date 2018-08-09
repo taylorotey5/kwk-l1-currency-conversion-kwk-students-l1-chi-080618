f59e33c1a7d1a60f4e8978a5a51d6405afa88b51
Taylor Otey <taylorotey5@gmail.com>
2018-08-09 20:22:32 +0000

    automatically backed up by learn

---
+++ calculator.rb
@@ -0,0 +1,6 @@
+def calculator (user_answers, num1, num2)
+  if user_answer == "add"
+    add(num1,num2)
+    elsif user_answer == "subtract"
+    subtract (num1, num2)
+    elsif user_answers == "multiply"
