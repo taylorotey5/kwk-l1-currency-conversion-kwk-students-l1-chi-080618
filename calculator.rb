243ed1e1bad0941386539c0025b3f588f4ae1f37
Taylor Otey <taylorotey5@gmail.com>
2018-08-09 20:25:27 +0000

    automatically backed up by learn

--- calculator.rb
+++ calculator.rb
@@ -4,3 +4,36 @@ def calculator (user_answers, num1, num2)
     elsif user_answer == "subtract"
     subtract (num1, num2)
     elsif user_answers == "multiply"
+    multiply(num1, num2)
+    elsif user_answers == "divide"
+    divide(num1, num2)
+  end
+end
+
+def add(num1, num2)
+  return num1 + num2
+end
+
+def subtract(num1, num2)
+  return num1 - num2
+end
+
+def multiply(num1, num2)
+  return num1 * num2
+end
+
+def divide(num1,num2)
+  return num1/num2
+end
+
+
+
+
+
+
+
+
+
+
+
+
