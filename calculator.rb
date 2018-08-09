3dc5fe2c298de8e799451dc5deb230b3a5127127
Taylor Otey <taylorotey5@gmail.com>
2018-08-09 20:29:12 +0000

    automatically backed up by learn

--- calculator.rb
+++ calculator.rb
@@ -2,7 +2,7 @@ def calculator (user_answers, num1, num2)
   if user_answer == "add"
     add(num1,num2)
     elsif user_answer == "subtract"
-    subtract (num1, num2)
+    subtract(num1, num2)
     elsif user_answers == "multiply"
     multiply(num1, num2)
     elsif user_answers == "divide"
@@ -27,13 +27,20 @@ def divide(num1,num2)
 end
 
 
+puts "WELCOME TO THE KLOSSY KALCULATOR!"
 
+puts "Would you like to add, divide, subtract, or multiply?"
+user_answers = gets.chomp
 
+puts "What is your first number?"
+num1 = gets.chomp.to.i
 
+#use gets chop.to_i because we cannot do math operations with strings
 
+puts "What is your second number?"
+num2 = gets.chomp.to_i
 
-
-
+puts calculator(user_answers, num1,num2)
 
 
 
